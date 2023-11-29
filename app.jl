bc1ffeddfcb5046d98f5ab724466e7f0dd671455
MahadAdnanGul <64431074+MahadAdnanGul@users.noreply.github.com>
2023-11-28 21:24:23 -0500

    Final Changes

--- app.jl
+++ app.jl
@@ -17,7 +17,7 @@ global dataPackage = "datasets"
 global set = "iris"
 global data = dataset(dataPackage, set)
 
-global plotTypes = ["scatter", "bar", "violin", "box"]
+global plotTypes = ["violin", "box"]
 
 global changingDatasets = false;
 
@@ -57,18 +57,21 @@ end
     @in datasetName = keys_array[1]
     @in xfeature = features[1]
     @in yfeature = features[2]
+    @in zfeature = features[1]
     @in gfeature = categorical_columns[1]
+    @in gfeature2 = categorical_columns[1]
     @in plotType = plotTypes[1]
     @out datatable = DataTable(data)
     @out datatablepagination = DataTablePagination(rows_per_page=50)
     @out irisplot = PlotData[]
+    @out barplot = PlotData()
+    @out boxplot = PlotData()
     @out clusterplot = PlotData[]
 
     @onchange isready, datasetName begin
         global changingDatasets = true
         println(datasetName)
         global dataPackage = get(dict, datasetName, "iris")
-        println(dataPackage)
         global set = datasetName
         global data = dataset(dataPackage, set)
 
@@ -78,23 +81,31 @@ end
         global features = [Symbol(c) for c in setdiff(names(data), categorical_columns)]
         xfeature = features[1]
         yfeature = features[2]
+        zfeature = features[1]
+        gfeature2 = categorical_columns[1]
         #categorical_columns = [Symbol(element) for element in categorical_columns]
         # Insert a new column with zeros
         global data = DataFrames.insertcols!(data, :Cluster => zeros(Int, size(data, 1)))
         global changingDatasets = false
         gfeature = categorical_columns[1]
 
+
     end
 
-    @onchange isready, xfeature, yfeature, no_of_clusters, no_of_iterations, gfeature, plotType begin
+    @onchange isready, xfeature, yfeature, no_of_clusters, no_of_iterations, gfeature, gfeature2, zfeature, plotType begin
         if (!changingDatasets)
-            println(xfeature)
-            println(yfeature)
             cluster(features, no_of_clusters, no_of_iterations)
             datatable = DataTable(data)
             grouped_df = DataFrames.combine(DataFrames.groupby(data, Symbol(gfeature)), DataFrames.nrow => :count)
-            println(grouped_df)
-            irisplot = plotdata(data, xfeature, yfeature; groupfeature=Symbol(gfeature), plottype=plotType)
+            #println(grouped_df)
+            #irisplot = PlotlyJS.plot(grouped_df, x=gfeature, y=:count, kind="bar")
+            println(gfeature)
+            println(gfeature2)
+            vec1 = DataFrames.getproperty(data, Symbol(gfeature2))
+            vec2 = DataFrames.getproperty(data, zfeature)
+            barplot = PlotData(x=DataFrames.getproperty(grouped_df, Symbol(gfeature)), y=DataFrames.getproperty(grouped_df, :count), plot="bar")
+            boxplot = PlotData(x=vec1, y=vec2, plot=plotType)
+            irisplot = plotdata(data, xfeature, yfeature; groupfeature=Symbol(gfeature))
             #irisplot = plotdata(data, xfeature, yfeature; groupfeature=Symbol(gfeature))
             clusterplot = plotdata(data, xfeature, yfeature; groupfeature=:Cluster)
         end
@@ -102,6 +113,6 @@ end
 end
 
 
-meta = Dict("og:title" => "Iris Clustering", "og:description" => "Dashboard to analyze k-means clustering results for iris dataset.", "og:image" => "/preview.png")
+meta = Dict("og:title" => "Data Clustering", "og:description" => "Dashboard to analyze k-means clustering results for any dataset.", "og:image" => "/preview.png")
 layout = DEFAULT_LAYOUT(meta=meta)
 @page("/", "app.jl.html", layout)
